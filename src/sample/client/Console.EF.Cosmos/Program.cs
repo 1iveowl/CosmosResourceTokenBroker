130a4b7d73c393f81094660e6e0753881e73cd65
Jasper H. Bojsen <liveowl@outlook.com>
2020-03-11 03:10:06 +0100

    wip

---
+++ src/sample/client/Console.EF.Cosmos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Console.EF.Cosmos
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
